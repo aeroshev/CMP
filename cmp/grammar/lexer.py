aeb6fa621d83aaa07736f930508897af07fc853b
Artem <aeroshev@mail.ru>
2021-03-15 23:03:57 +0300

    Rewrite lexer part 1

--- cmp/grammar/lexer.py
+++ cmp/grammar/lexer.py
@@ -10,49 +10,44 @@ class Lexer:
         "for": "FOR",
         "while": "WHILE",
         "break": "BREAK",
-        "continue": "CONTINUE",
+        # "continue": "CONTINUE",
         # Conditions
         "if": "IF",
         "else": "ELSE",
         "elseif": "ELSEIF",
         "end": "END",
         # Catch exceptions
-        "try": "TRY",
-        "catch": "CATCH",
+        # "try": "TRY",
+        # "catch": "CATCH",
         # Function
         "function": "FUNCTION",
         "return": "RETURN",
         # Switch
-        "switch": "SWITCH",
-        "case": "CASE",
+        # "switch": "SWITCH",
+        # "case": "CASE",
         # Other
         "global": "GLOBAL",
-        "end_unwind_protect": "END_UNWIND_PROTECT",
-        "unwind_protect": "UNWIND_PROTECT",
-        "unwind_protect_cleanup": "UNWIND_PROTECT_CLEANUP",
-        "otherwise": "OTHERWISE",
-        "persistent": "PERSISTENT",
+        # "end_unwind_protect": "END_UNWIND_PROTECT",
+        # "unwind_protect": "UNWIND_PROTECT",
+        # "unwind_protect_cleanup": "UNWIND_PROTECT_CLEANUP",
+        # "otherwise": "OTHERWISE",
+        # "persistent": "PERSISTENT",
         "clear": "CLEAR",
     }
 
     # All tokens
     tokens = tuple([
-        "COLON",
         # Logic operations
         "LE_OP", "LT_OP", "GE_OP", "GT_OP", "EQ_OP", "NE_OP",
         "AND_AND", "OR_OR",
         # Arithmetic operations
-        "MUL", "POW", "DIV", "DIV_DIV", "TRANSPOSE",
+        "DIV_DIV", "TRANSPOSE",
         # Plus operations
-        "PLUS", "PLUS_EQ", "PLUS_PLUS",
+        "PLUS_EQ", "PLUS_PLUS",
         # Minus operations
-        "MINUS", "MINUS_EQ", "MINUS_MINUS",
+        "MINUS_EQ", "MINUS_MINUS",
         # Dot operations
         "DOT", "DOT_DIV", "DOT_DIV_EQ", "DOT_EXP", "DOT_MUL", "DOT_MUL_EQ",
-        # Brackets
-        "LPAREN", "RPAREN",
-        # Other
-        "ASSIGN",
         # Complex objects
         "ID",
         "NUMBER",
@@ -65,13 +60,27 @@ class Lexer:
     t_ignore_WHITESPACE = r"\s+"
     t_ignore_COMMENT = r"\%.*"
 
+    # literals
+    literals = [
+        '~', ';', ',', ':', '=', '(',
+        ')', '[', ']', '&', '-', '+',
+        '*', '/', '\\', '>', '<', '|'
+    ]
+
     # Regular expressions for complex tokens
     newline = r"\n+"
-    number = r"\d+"
-    identifier = r"[a-zA-Z_][a-zA-Z_0-9]*"
+
+    D = r"[0-9]"
+    L = r"[a-zA-Z]_"
+    E = fr"[DdEe][+-]?{D}+"
+
+    constant_1 = fr'{D}+({E})?'
+    constant_2 = fr'{D}*"."{D}+({E})?'
+    constant_3 = fr'{D}+"."{D}*({E})?'
+
+    identifier = fr'{L}({L}|{D})*'
 
     # Regular expressions for simple tokens
-    t_COLON = r":"
     # Logic operations
     t_LE_OP = r"<="
     t_LT_OP = r"\<"
@@ -82,31 +91,21 @@ class Lexer:
     t_AND_AND = r"\&\&"
     t_OR_OR = r"\|\|"
     # Arithmetic operations
-    t_MUL = r"\*"
-    t_POW = r"\^"
-    t_DIV = r"/"
-    t_DIV_DIV = r"\\"
     t_TRANSPOSE = r"'"
     # Plus operations
-    t_PLUS = r"\+"
     t_PLUS_EQ = r"\+="
     t_PLUS_PLUS = r"\+\+"
     # Minus operations
-    t_MINUS = r"\-"
     t_MINUS_EQ = r"\-="
     t_MINUS_MINUS = r"\--"
     # Dot operations
-    t_DOT = r"\."
-    t_DOT_DIV = r"\./"
-    t_DOT_DIV_EQ = r"\./="
-    t_DOT_EXP = r"\.\^"
-    t_DOT_MUL = r"\.\*"
-    t_DOT_MUL_EQ = r"\.\*="
+    # t_DOT = r"\."
+    # t_DOT_DIV = r"\./"
+    # t_DOT_DIV_EQ = r"\./="
+    # t_DOT_EXP = r"\.\^"
+    # t_DOT_MUL = r"\.\*"
+    # t_DOT_MUL_EQ = r"\.\*="
     # Brackets
-    t_LPAREN = r"\("
-    t_RPAREN = r"\)"
-
-    t_ASSIGN = r"="
 
     def __init__(self, **kwargs) -> None:
         self._lexer = lex(module=self, optimize=1, **kwargs)
@@ -116,16 +115,15 @@ class Lexer:
         print(f"Illegal character {token_.value[0]}")
         token_.lexer.skip(1)
 
-    @TOKEN(identifier)
-    def t_ID(self, token_: LexToken) -> LexToken:
-        """Token [a-zA-Z_]+"""
-        token_.type = Lexer.keywords.get(token_.value, "ID")
-        return token_
+    @TOKEN(constant_1 + constant_2 + constant_3)
+    def t_CONSTANT(self, token_: LexToken) -> LexToken:
+        """"""
+        ...
 
-    @TOKEN(number)
-    def t_NUMBER(self, token_: LexToken) -> LexToken:
-        """Token [digit]+"""
-        token_.value = int(token_.value)
+    @TOKEN(identifier)
+    def t_IDENTIFIER(self, token_: LexToken) -> LexToken:
+        """"""
+        token_.type = Lexer.keywords.get(token_.value, "ID")
         return token_
 
     @TOKEN(newline)
